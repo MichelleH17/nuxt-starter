bde03cd1c441482cb9d296b65dafc7580afc5e35
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-26 12:48:01 +0200

    setup eslint, lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
