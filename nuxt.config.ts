ecb17a39ce081761dae480724c2c386a95fcdeb0
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-26 13:32:43 +0200

    add tailwindcss, vscode recomendations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
@@ -9,9 +12,15 @@ export default defineNuxtConfig({
     "@nuxt/icon",
     "@nuxt/image",
   ],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
